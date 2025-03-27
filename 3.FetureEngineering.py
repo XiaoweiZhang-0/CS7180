faa3b5f04581da37610425f4965c5343a8522c95
Xiaowei Zhang <blackcatecho1998@gmail.com>
2025-03-27 16:58:28 -0700

    add music id, save X and Y

--- 3.FetureEngineering.py
+++ 3.FetureEngineering.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 df = pd.read_csv('tiktok_dataset.csv')
 
-df['desc_len'] = df['desc'].astype(str).apply(len)
+# df['desc_len'] = df['desc'].astype(str).apply(len)
 
-df['num_hashtags'] = df['desc'].astype(str).str.count('#')
+# df['num_hashtags'] = df['desc'].astype(str).str.count('#')
 
 df['verified'] = df['author.verified'].astype(int)
 
 df['hour'] = pd.to_datetime(df['createTime']).dt.hour
 df['weekday'] = pd.to_datetime(df['createTime']).dt.weekday
 
-df['is_original_sound'] = df['music.title'].astype(str).apply(lambda x: int('original sound' in x.lower()))
+df['music_id'] = df['music.id'].astype(str)
 
 # TODO:
 # 1. Description: change length to transformer evaluated attitude: positive, negative, neutral
@@ -21,12 +21,13 @@ df['is_original_sound'] = df['music.title'].astype(str).apply(lambda x: int('ori
 
 X = df[[
     'video.duration',
-    'desc_len',
-    'num_hashtags',
+    # 'desc_len',
+    # 'num_hashtags',
     'verified',
     'hour',
     'weekday',
-    'is_original_sound'
+    'music_id',
+    # 'is_original_sound'
 ]]
 
 # build a target variable y by combining 4 features with different weights
@@ -52,8 +53,8 @@ df['engagement_score'] = (
 y = df['engagement_score']
 
 df.to_csv('tiktok_dataset_with_engagement_score.csv', index=False)
-# X.to_csv('X_features.csv', index=False)
-# y.to_csv('y_target.csv', index=False)
+X.to_csv('X_features.csv', index=False)
+y.to_csv('y_target.csv', index=False)
 
 print("✅")
 print(f"X shape: {X.shape}")
