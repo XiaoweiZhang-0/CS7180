339fe62c1b3164b823aafdb473343e6dfa4b9464
Xiaowei Zhang <blackcatecho1998@gmail.com>
2025-04-10 16:48:06 -0700

    add bert embedding

--- text_embedding.py
+++ text_embedding.py
@@ -1,8 +1,9 @@
 import json
 
+from sentence_transformers import SentenceTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from data_preprocessing import preprocess_text
+from data_preprocessing import preprocess_text, remove_hashtags_and_mentions
 
 
 def tf_idf(file_name):
@@ -27,5 +28,28 @@ def tf_idf(file_name):
     return tfidf_matrix
 
 
+def bert(file_name):
+    # Load the json file
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    # Extract the 'desc' field
+    descs = [entry["desc"] for entry in data]
+
+    # Extract the 'desc' field
+    descs = [remove_hashtags_and_mentions(desc) for desc in descs]
+
+    # Create a BERT model
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    # Encode the text
+    embeddings = model.encode(descs)
+
+    print("BERT embeddings shape:", embeddings.shape)
+
+    return embeddings
+
+
 if __name__ == "__main__":
-    tf_idf("subset_10000.json")
+    # tf_idf("subset_10000.json")
+    bert("subset_10000.json")
